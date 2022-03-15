fcc96dd1ba5c291cc77b6ffad8d4181c9e95a977
AntenehAdmasu <antenehadmasu8@gmail.com>
2022-03-15 18:18:21 +0300

    Add metadata

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x631312F2a0749AdB0CC8ba472cF1347379e49341";
+const WHITELIST_CONTRACT_ADDRESS = "0x30C57f2f7218406c7930f51f530CCB37652A6129";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-gray.vercel.app/api/";
 
