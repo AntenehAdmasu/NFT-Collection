4162320b04c995671f3a140e55f8f56050f017d2
AntenehAdmasu <antenehadmasu8@gmail.com>
2022-03-15 17:59:13 +0300

    Make the NFT available on OpenSea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x31fca48304B99AA1Add5902356d3163b97245189";
+const WHITELIST_CONTRACT_ADDRESS = "0x631312F2a0749AdB0CC8ba472cF1347379e49341";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-gray.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
